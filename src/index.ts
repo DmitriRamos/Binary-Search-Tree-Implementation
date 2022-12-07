bc8408e4dad645a10e1be6ae61bdc500128867f2
Dmitri Ramos <odreastonbjj@gmail.com>
2022-12-07 14:54:29 -0700

    git init

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+//console.log("testing");
+class TreeNode {
+  data: number;
+  leftChild: TreeNode;
+  rightChild: TreeNode;
+  constructor(data, leftChild, rightChild) {
+    this.data = data;
+    this.leftChild = leftChild;
+    this.rightChild = rightChild;
+  }
+}
+
+class Tree {
+  method;
+}
